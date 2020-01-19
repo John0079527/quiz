e07b59a43d249e612d711784b051c0f3d2a03647
LAPTOP-OS7NO7L8\liche <licheng@infinit.com.sg>
2020-01-19 19:55:20 +0800

    Refactor Animal abstract class

--- src/main/java/com/sing/quiz/QuizApplication.java
+++ src/main/java/com/sing/quiz/QuizApplication.java
@@ -3,9 +3,15 @@ package com.sing.quiz;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-class Animal {
-	void walk() {
-		System.out.println("I am walking");
+abstract class Animal {
+	String name = "Animal";
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
 	}
 }
 
@@ -39,10 +45,10 @@ public class QuizApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(QuizApplication.class, args);
-		Bird bird = new Bird();
+/*		Bird bird = new Bird();
 		bird.walk();
 		bird.fly();
-		bird.sing();
+		bird.sing();*/
 	}
 
 }
