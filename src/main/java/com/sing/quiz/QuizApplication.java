f060071234b0c6e90cc7d993fe246b51f736e7e2
LAPTOP-OS7NO7L8\liche <licheng@infinit.com.sg>
2020-01-19 19:31:26 +0800

    Revise Animal class to abstract

--- src/main/java/com/sing/quiz/QuizApplication.java
+++ src/main/java/com/sing/quiz/QuizApplication.java
@@ -3,9 +3,15 @@ package com.sing.quiz;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-class Animal {
-	void walk() {
-		System.out.println("I am walking");
+abstract class Animal {
+	String name = "Animal";
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
 	}
 }
 
@@ -36,7 +42,7 @@ public class QuizApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(QuizApplication.class, args);
 		Bird bird = new Bird();
-		bird.walk();
+		//bird.walk();
 		bird.fly();
 		bird.sing();
 	}
