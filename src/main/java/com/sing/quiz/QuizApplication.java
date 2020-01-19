605f783840453dee7bf47408ebbbb128b6999427
LAPTOP-OS7NO7L8\liche <licheng@infinit.com.sg>
2020-01-19 19:37:41 +0800

    Redefine Bird Class, inheriting Animal and implements IWalk and ISing

--- src/main/java/com/sing/quiz/QuizApplication.java
+++ src/main/java/com/sing/quiz/QuizApplication.java
@@ -3,6 +3,10 @@ package com.sing.quiz;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.singtel.test.test.Animal;
+import com.singtel.test.test.ISing;
+import com.singtel.test.test.IWalk;
+
 abstract class Animal {
 	String name = "Animal";
 
@@ -29,6 +33,19 @@ interface IFly {
 
 interface ISing {
 	void sing();
+}
+
+class Bird extends Animal implements IWalk, ISing {
+
+	@Override
+	public void sing() {
+		System.out.println("I am singing");
+	}
+
+	@Override
+	public void walk() {
+		System.out.println("I am walking");
+	}
 }
 
 @SpringBootApplication
