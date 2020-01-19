68912dd4c5b2fd4c81e9935269ada123906d7429
LAPTOP-OS7NO7L8\liche <licheng@infinit.com.sg>
2020-01-19 20:00:49 +0800

    Model Duck inherting Bird and implements ISwim, IFly

--- src/main/java/com/sing/quiz/QuizApplication.java
+++ src/main/java/com/sing/quiz/QuizApplication.java
@@ -45,6 +45,24 @@ class Bird extends Animal implements IWalk, ISing {
 	
 }
 
+class Duck extends Bird implements ISwim, IFly {
+	
+	@Override
+	public void sing() {
+		System.out.println("Quack, quack");
+	}
+	
+	@Override
+	public void swim() {
+		System.out.println("I can swim");
+	}
+
+	@Override
+	public void fly() {
+		System.out.println("I am flying");
+	}
+}
+
 @SpringBootApplication
 public class QuizApplication {
 
