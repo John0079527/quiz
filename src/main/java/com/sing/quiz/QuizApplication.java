17945150aefb17ef79bac8e9e012bbb7d2042cef
LAPTOP-OS7NO7L8\liche <licheng@infinit.com.sg>
2020-01-19 19:49:26 +0800

    init commit, question 1

--- src/main/java/com/sing/quiz/QuizApplication.java
+++ src/main/java/com/sing/quiz/QuizApplication.java
@@ -3,48 +3,18 @@ package com.sing.quiz;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.singtel.test.test.Animal;
-import com.singtel.test.test.ISing;
-import com.singtel.test.test.IWalk;
-
-abstract class Animal {
-	String name = "Animal";
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-}
-
-interface ISwim {
-	void swim();
-}
-
-interface IWalk {
-	void walk();
-}
-
-interface IFly {
-	void fly();
-}
-
-interface ISing {
-	void sing();
-}
-
-class Bird extends Animal implements IWalk, ISing {
-
-	@Override
-	public void sing() {
-		System.out.println("I am singing");
-	}
-
-	@Override
-	public void walk() {
+class Animal {
+	void walk() {
 		System.out.println("I am walking");
+	}
+}
+
+class Bird extends Animal {
+	void fly() {
+		System.out.println("I am flying");
+	}
+	void sing() {
+		System.out.println("I am Singing");
 	}
 }
 
@@ -53,10 +23,10 @@ public class QuizApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(QuizApplication.class, args);
-/*		Bird bird = new Bird();
-		//bird.walk();
+		Bird bird = new Bird();
+		bird.walk();
 		bird.fly();
-		bird.sing();*/
+		bird.sing();
 	}
 
 }
